1c8b2b0a853d093646752abaa1b00c7b949f5b98
Sandhya Sanil <sandhyasanil@gmail.com>
2026-04-04 18:28:50 +0530

    Updated __init__.py to include new modules

--- SMC.py
+++ SMC.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-class ExtinctionCurve:
+class SMCExtinction:
     
     from astropy import units as u
     """
--- __init__.py
+++ __init__.py
@@ -1,2 +1,4 @@
-from .model import ExtinctionCurve
-__all__ = ['ExtinctionCurve']
+from .LMC import LMCExtinction
+from .LMC2 import LMC2Extinction
+from .SMC import SMCExtinction
+__all__ = ['LMCExtinction', 'LMC2Extinction', 'SMCExtinction']
